bd454aede38c0c9724e3a6dcdf1022d8a5739507
Alex Loftus <atloftus@wisc.edu>
2019-09-24 11:46:26 -0500

    Added the Killers library and fully fleshed out the intro decision tree

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Diagnostics;
 using System.Collections.Generic;
 using Hunters;
+using Killers;
 
 
 namespace LIHunter
@@ -21,22 +22,121 @@ namespace LIHunter
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.Write("Would you like to update Indeed(y/n)? ");
-            string inInput = Console.ReadLine();
-            Console.Write("Would you like to update LinkedIn(y/n)? ");
-            string liInput = Console.ReadLine();
+            Console.Write("Welcome to the Application Manger!");
+            Console.Write("Would you like to hunt jobs(y/n)? ");
+            string huntInput = Console.ReadLine();
+            Console.Write("Would you like to kill jobs(y/n)? ");
+            string killInput = Console.ReadLine();
 
-            if ((inInput.Contains('y')) && (liInput.Contains('y'))) runBoth();
-            else if (inInput.Contains('y')) runIndeed();
-            else if (liInput.Contains('y')) runLinkedIn();
-                               
+            if ((huntInput.Contains('y')) && (killInput.Contains('y'))) runAll();
+            else if (huntInput.Contains('y')) huntJobs();
+            else if (killInput.Contains('y')) killJobs();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void runAll()
+        {
+            huntJobs();
+            killJobs();
         }
 
 
         /// <summary>
         /// 
         /// </summary>
-        public static void runLinkedIn()
+        public static void killJobs()
+        {
+            Console.Write("Would you like to kill Indeed(y/n)? ");
+            string inInput = Console.ReadLine();
+            Console.Write("Would you like to kill LinkedIn(y/n)? ");
+            string liInput = Console.ReadLine();
+
+            if ((inInput.Contains('y')) && (liInput.Contains('y'))) runBothKills();
+            else if (inInput.Contains('y')) runIndeedKill();
+            else if (liInput.Contains('y')) runLinkedInKill();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void runBothKills()
+        {
+            runLinkedInKill();
+            runIndeedKill();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void runLinkedInKill()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine("Welcome to LIKiller! Initializing LinkedIn Service...");
+            Killers.LIKiller liKiller = new LIKiller();
+            Console.WriteLine("Successfully initialized LinkedIn Killer.");
+
+            Console.WriteLine("Applying to jobs...");
+            int[] results = liKiller.applyToJobs();
+            Console.WriteLine("Completed all LinkedIn Searchs!");
+
+            Console.WriteLine("Initializing Google Drive Service...");
+            Killers.GoogleDriveService googleDriveService = new Killers.GoogleDriveService();
+            Console.WriteLine("Successfully initialized Google Drive Service.");
+
+            Console.WriteLine("Writing application results to your google sheet...");
+            //TODO: need to add calls to udpdate present entries
+            string updateResponse = googleDriveService.CreateGoogleSheetsLIJobEntries(googleDriveService.Jobs);
+            Console.WriteLine("Completed writing results to google sheets.");
+            stopwatch.Stop();
+
+            Console.WriteLine("Thank you for using LIKiller!");
+            Console.WriteLine("-----SESSION STATS-----");
+            Console.WriteLine("Number of SUCCESSful applications on LinkedIn: " + results[0]);
+            Console.WriteLine("Number of FAILed applications on LinkedIn: " + results[1]);
+            Console.WriteLine("Application Run Time: {0:mm\\:ss}", stopwatch.Elapsed);
+            Console.WriteLine("-----------------------");
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void runIndeedKill()
+        {
+            //TODO: Finish this method
+        }
+
+
+
+
+
+
+
+        public static void huntJobs()
+        {
+            Console.Write("Would you like to update Indeed(y/n)? ");
+            string inInput = Console.ReadLine();
+            Console.Write("Would you like to update LinkedIn(y/n)? ");
+            string liInput = Console.ReadLine();
+
+            if ((inInput.Contains('y')) && (liInput.Contains('y'))) runBothHunts();
+            else if (inInput.Contains('y')) runIndeedHunt();
+            else if (liInput.Contains('y')) runLinkedInHunt();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void runLinkedInHunt()
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -70,7 +170,7 @@ namespace LIHunter
         /// <summary>
         /// 
         /// </summary>
-        public static void runIndeed()
+        public static void runIndeedHunt()
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -104,10 +204,10 @@ namespace LIHunter
         /// <summary>
         /// 
         /// </summary>
-        public static void runBoth()
+        public static void runBothHunts()
         {
-            runLinkedIn();
-            runIndeed();
+            runLinkedInHunt();
+            runIndeedHunt();
         }
     }
 }
